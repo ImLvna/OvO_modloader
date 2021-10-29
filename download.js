d343c73b4648c734311c999a74d1c8eb4a2afaf1
ImLvna <44179559+ImLvna@users.noreply.github.com>
2021-10-29 17:54:43 -0400

    Ver switcher, better string handling, args, etc

--- download.js
+++ download.js
@@ -3,17 +3,19 @@ const fs = require('fs');
 const request = require('request');
 
 
-module.exports.getVData = (ver) => {return new Promise(resolve => {
+module.exports.getVData = (ver) => {return new Promise((resolve, reject) => {try{
 	request(`https://dedragames.com/games/ovo/${ver}/offline.js`, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			if(body.charCodeAt(0) === 65279) body = body.trim()
 			resolve(JSON.parse(body))
+		} else {
+			reject(error || response.statusCode)
 		}
 	})
-})};
+}catch(e){reject(e)}})};
 
 
-module.exports.downloadGame = (ver) => {return new Promise(resolve => {
+module.exports.downloadGame = (ver) => {return new Promise((resolve, reject) => {try{
 	const gameUrl = `https://dedragames.com/games/ovo/${ver}/`
 	module.exports.getVData(ver).then(body => {
 		let fileList = body.fileList;
@@ -26,7 +28,7 @@ module.exports.downloadGame = (ver) => {return new Promise(resolve => {
 			dirs.pop()
 			dirs = dirs.join('/')
 			fs.mkdirSync(`gameFiles/${ver}/${dirs}`, { recursive: true })
-			const file = fs.createWriteStream(`gameFiles/${ver}/` + i);
+			const file = fs.createWriteStream(`gameFiles/${ver}/${i}`);
 			const request = https.get(gameUrl + i, function(response) {
 				response.pipe(file);
 				file.on('close', () => {
@@ -47,5 +49,5 @@ module.exports.downloadGame = (ver) => {return new Promise(resolve => {
 				})
 			});
 		})
-	})
-})};
+	}, err => {reject(err)})
+}catch(e){reject(e)}})};
--- server.js
+++ server.js
@@ -7,94 +7,124 @@ var app = express();
 
 
 
-//CONFIG
+//DEFAULT CONFIG
 const enableMods = true;
 //Allow overwriting files
 //Enable this and copy the file to the overwrites folder
 //This will force OvO to use the overwritten file
 const enableOverwrites = true;
 //Version of the game when none is specified
-const defaultVersion = 'CrashTest';
+const gameVersion = 'CrashTest';
 
 //Order of loading
-//1. If mods are enabled, load all the mods' .js files into index.html
-//2. If overwrites are enabled, send all the overwrites'files
-//3. Load the default files
+//1. If we are using debug mode, load the debug overwrites
+//2. If mods are enabled, load all the mods' .js files for that version into index.html
+//3. If mods are enabled, load all the mods from mods/default into index.html
+//4. If overwrites are enabled, send all the overwrites'files
+//5. Load the default files
 
 
 
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
-
+let debug = false
 const argv = yargs(hideBin(process.argv))
     .usage('Usage: $0 [options]')
-    .command('$0', 'default')
-    .example('$0 -v 1.4.4', 'Play version 1.4.4')
-    .example('$0 -v CrashTest', 'Play version CrashTest')
-    .example('$0', 'Play the default version set in server.js')
-    .version(false)
-    .alias('v', 'version')
-    .nargs('v', 1)
-    .describe('v', 'The version to play')
-    .string('v')
-    .default('v', defaultVersion)
+        .example('$0 -v 1.4.4', 'Play version 1.4.4')
+        .example('$0 -v CrashTest', 'Play version CrashTest')
+        .example('$0', 'Play the default version set in server.js')
+        .example('$0 debug', 'Output debug files for default version')
+        .example('$0 debug 1.4.4', 'Output debug files for 1.4.4')
+    .command('$0', 'Run/download the game') 
+        .version(false)
+        .alias('v', 'version')
+            .nargs('v', 1)
+            .describe('v', 'The version to play')
+            .string('v')
+            .default('v', gameVersion)
+        .alias('m', 'mods')
+            .describe('m', 'Enable mods')
+            .boolean('m')
+            .default('m', enableMods)
+        .alias('o', 'overwrites')
+            .describe('o', 'Enable overwrites')
+            .boolean('o')
+            .default('o', enableOverwrites)
+        .alias('d', 'debug')
+            .describe('d', 'Use debug overwrites')
+            .boolean('d')
+            .default('d', false)
+    .command(['debug', 'd'], 'Output various debug files', noop, ()=>{debug = true})
+        .version(false)
+        .alias('v', 'version')
+            .nargs('v', 1)
+            .describe('v', 'The version to debug')
+            .string('v')
+            .default('v', gameVersion)
     .help('h')
-    .alias('h', 'help')
-    .nargs('h', 0)
+        .alias('h', 'help')
+        .nargs('h', 0)
     .epilog('LvnaLoader')   
     .argv;
 
 
-
-(async () => {
-
-    
+(async () => {if(!debug){
+    fs.mkdirSync(`mods/${argv.v}`, {recursive: true});
+    fs.mkdirSync('mods/default', {recursive: true});
+    fs.mkdirSync(`overwrites/${argv.v}`, {recursive: true});
 
     //Check if the game is already downloaded
-    if (!fs.existsSync('./gameFiles/' + argv.v + '/versionData.json')) {
+    if (!fs.existsSync(`gameFiles/${argv.v}/versionData.json`)) {
         console.log('Game not found, downloading...');
-        await downloadGame(argv.v);
-    } else {
-        let vData = JSON.parse(fs.readFileSync('./gameFiles/' + argv.v + '/versionData.json', 'utf8'));
+        try{
+            await downloadGame(argv.v);
+        }catch(e){console.error('Download Error. Do you have internet? ' + e)}
+    } else {try{
+        let vData = JSON.parse(fs.readFileSync(`gameFiles/${argv.v}/versionData.json`, 'utf8'));
         let onlineVData = await getVData(argv.v);
         if(vData.ver !== onlineVData.version) {
             console.log('Update Required. Deleting old files...');
-            fs.rmdirSync('./gameFiles/' + argv.v, { recursive: true });
+            fs.rmdirSync(`gameFiles/${argv.v}`, { recursive: true });
             await downloadGame(argv.v);
         }
-    }
+    }catch(e){console.error('Download Error. Do you have internet? ' + e)}}
 
-    if(enableMods) {
+    if(argv.m) {
         let modNames = []
         let modStr = '\n\n	<!--- lvnaMod Loader Mods --->'
-        fs.readdirSync('mods').forEach(file => {
+        fs.readdirSync(`mods/${argv.v}`).forEach(file => {
+            if(!file.startsWith('_')) modNames.push(file)
+        });
+        fs.readdirSync('mods/default').forEach(file => {
             if(!file.startsWith('_')) modNames.push(file)
         });
         modNames.forEach(mod => {
-            modStr += '\n	<script src="' + mod + '"></script>'
+            modStr += `\n	<script src="${mod}"></script>`
         })
         modStr += '\n	<!--- lvnaMod Loader Mods --->'
 
         function indexHandler(req, res) {
-            fs.readFile(path.join(__dirname, 'gameFiles', argv.v ,'index.html'), 'utf8', function(err, data) {
+            fs.readFile(`gameFiles/${argv.v}/index.html`, 'utf8', function(err, data) {
                 if (err) {
                     res.sendStatus(404);
                 } else {
-                    res.send(data.replace('<script src="c2runtime.js"></script>', '<script src="c2runtime.js"></script>'+modStr));
+                    res.send(data.replace(/(<script src="c2runtime\.js"><\/script>)/, `$1${modStr}`));
                 }
             });
         }
-        app.get("/index.html", indexHandler);
-        app.get("/", indexHandler);
-        app.get("/index", indexHandler);
+        app.get('/index.html', indexHandler);
+        app.get('/', indexHandler);
+        app.get('/index', indexHandler);
 
 
-        app.use(express.static(__dirname + '/mods'));
+        app.use(express.static(`mods/${argv.v}`));
+        app.use(express.static('mods/default'));
     }
 
-    if(enableOverwrites) app.use(express.static(__dirname + '/overwrites'));
+    if(argv.o) app.use(express.static('overwrites'));
+    if(argv.d) app.use(express.static(`debug/${argv.v}`));
 
-    app.use(express.static(__dirname + '/gameFiles/' + argv.v));
+    app.use(express.static(`gameFiles/${argv.v}`));
 
     //VERY shitty string manip
     //none: 'running at http...'
@@ -102,12 +132,25 @@ const argv = yargs(hideBin(process.argv))
     //overwrite: 'running with overwrites at http...'
     //both: 'running with mods and overwrites at http...'
     let statusStr = '';
-    if(enableMods) {
+    if(argv.m) {
         statusStr = 'with mods ';
-        if(enableOverwrites) statusStr += 'and overwrites '
-    } else if(enableOverwrites) statusStr = 'with overwrites '
+        if(argv.o) statusStr += 'and overwrites '
+    } else if(argv.o) statusStr = 'with overwrites '
     app.listen(8080, () => {
         console.log(`OvO ${argv.v} running ${statusStr}at http://localhost:8080`)
     })
 
-})();
+} else {
+    let dataJson = fs.readFileSync(`gameFiles/${argv.v}/data.js`, 'utf8')
+    if(dataJson.charCodeAt(0) === 65279) dataJson = dataJson.trim()
+    dataJson = JSON.parse(dataJson)
+    dataJson = JSON.stringify(dataJson, null, 2)
+    fs.mkdirSync(`debug/${argv.v}`, { recursive: true })
+    fs.writeFileSync(`debug/${argv.v}/gameData.json`, dataJson)
+    fs.copyFileSync(`gameFiles/${argv.v}/c2runtime.js`, `debug/${argv.v}/c2runtime.js`)
+    //File doesnt exist in 1.4.3
+    try{fs.copyFileSync(`gameFiles/${argv.v}/adconfig.json`, `debug/${argv.v}/adconfig.json`)}catch(e){}
+    fs.copyFileSync(`gameFiles/${argv.v}/versionData.json`, `debug/${argv.v}/versionData.json`)
+}})()
+
+function noop() {};
